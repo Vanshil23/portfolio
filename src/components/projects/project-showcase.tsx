ec14aefc073b5af7e1710845aee00b2a3d9eeaf5
Vanshil23 <shethvanshil1234@gmail.com>
2025-05-31 15:30:14 +0530

    fghj

--- src/components/projects/project-showcase.tsx
+++ src/components/projects/project-showcase.tsx
@@ -1,13 +1,8 @@
-
 export default function ProjectShowcase() {
-
   return (
     <section className="overflow-hidden">
       <div className="relative mx-auto max-w-7xl">
-       
-        <div className="relative right-0 top-0 hidden lg:block">
-          
-        </div>
+        <div className="relative right-0 top-0 hidden lg:block"></div>
       </div>
     </section>
   );
